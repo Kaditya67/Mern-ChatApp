71a3a51868d74f75f2d44051299bd92b412ce4c4
Kaditya67 <322adityakumar0004@dbit.in>
2024-06-02 19:39:03 +0530

    Conversations Users loading

--- frontend/src/components/sidebar/Conversation.jsx
+++ frontend/src/components/sidebar/Conversation.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
 
-const Conversation = () => {
+const Conversation = ({ conversation, lastIdx, emoji }) => {
   return (
     <>
       <div className='flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer'>
         <div className='avatar online'>
           <div className='w-12 rounded-full'>
-            <img src='path/to/user-avatar.jpg' alt='user-avatar' />
+            <img src={conversation.profilePic} alt='user-avatar' />
           </div>
         </div>
         <div className='flex flex-col flex-1'>
           <div className='flex gap-3 justify-between'>
-            <p className='font-bold text-gray-200'>Aditya Ojha</p>
-            <span className='text-xl'>emoji</span>
+            <p className='font-bold text-gray-200'>{conversation.fullName}</p>
+            <span className='text-xl'>{emoji}</span>
           </div>
         </div>
       </div>
-      <div className='divider my-0 py-0 h-1' />
+      {!lastIdx && <div className='divider my-0 py-0 h-1' />}
     </>
   );
 };
